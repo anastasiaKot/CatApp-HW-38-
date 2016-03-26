e527f109498ec4aaa5f4d1d07e7949f42573f595
anastasiashpak <anastasia.shpak@ymedialabs.com>
2016-03-26 16:38:32 -0700

    JUnit assertions and SR

--- src/test/java/core/CatAppTest.java
+++ src/test/java/core/CatAppTest.java
@@ -13,95 +13,67 @@ import org.junit.Test;
 public class CatAppTest {
 
 	@BeforeClass
-	public static void setUpBeforeClass() throws Exception 
-	{
-		System.out.println("BeforeClass method will be executed before first test method starts");
-	}
-
+	public static void BeforeClass() throws Exception {}
 	@AfterClass
-	public static void tearDownAfterClass() throws Exception 
-	{
-		System.out.println("AfterClass method will be executed after last test method completed");
-
-	}
-
+	public static void AfterClass() throws Exception {}
 	@Before
-	public void setUp() throws Exception 
-	{
-	       System.out.println("Before method will execute before every test method");
-	}
-
+	public void before() throws Exception {}
 	@After
-	public void tearDown() throws Exception 
-	{
-	       System.out.println("After method will execute after every test method");
-	}
-
+	public void after() throws Exception {}
+	
 	@Test
 	public void test_01_assertEquals_Positive() 
 	{
-		String s = "ok";
-		
-	    assertEquals("Test_01_assertEquals_Positive", "ok", s);
+		assertEquals("String not the same", "Testing JUnit 4", App.s);
 	}
 	@Test
 	public void test_02_asertEquals_Negative() 
 	{
-		String s = "ok";
-		
-	    assertEquals("Test_01_assertEquals_Positive", "bla", s);
+		assertEquals("String not the same", "Testing JUnit 44", App.s);
 	}
 	@Ignore
 	@Test
 	public void test_03_assertEquals_Ignored()
 	{
-		String d = "bla";
-		assertEquals("Test_02_assertEquals_Positive", "ok", d);
+		assertEquals("String not the same", "Testing JUnit 4", App.s);
 	}
 	
 	@Test
 	public void test_04_assertSame_Positive()
 	{
-		Integer s = 125;
-		assertSame("Should be the same", 125, s);
+		assertSame("Integer not the same.", 55,App.i);
 	}
 	@Test
 	public void test_05_assertSame_Negative()
 	{
-		Integer s = 125;
-		assertSame("Should be the same", 14, s);
+		assertSame("Integer not the same.", 5,App.i);
 	}
 	@Ignore
 	@Test
 	public void test_06_assertSame_Ignored()
 	{
-		Integer s = 125;
-		assertSame("Should be the same", 125, s);
+		assertSame("Integer not the same.", 55,App.i);
 	}
 	@Test
 	public void test_07_assertFalse_Positive()
 	{
-		//Boolean F;
-		assertFalse("Yes, it is false!", 5<2);
+		assertTrue("Boolean should be false", App.f);
 	}
 
 	@Test
 	public void test_08_assertFalse_Negative()
 	{
-		//Boolean F;
-		assertFalse("No, it is not false!", 5>2);
+		assertTrue("Boolean should be false", App.t);
 	}
 	@Test
 	public void test_09_assertTrue_positive()
 	{
-		//Boolean F= true;
-		assertTrue("Yes, it is true!", 5>2);
+		assertTrue("Boolean should be true", App.f);
 	}
 	@Test
 	public void test_10_assertTrue_negative()
 	{
-		//Boolean F= true;
-		assertTrue("No, it is not true!", 5<2);
+		assertTrue("Boolean should be true", App.t);
 	}
 	
 }
